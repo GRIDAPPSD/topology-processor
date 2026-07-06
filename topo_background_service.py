05ef2600303a47c1a224fc4ed7ea276dec0f547d
poorva1209 <poorva1209@gmail.com>
2026-07-06 14:56:30 -0700

    updated topology background service to use latest cimgraph

--- topo_background_service.py
+++ topo_background_service.py
@@ -1,14 +1,26 @@
 import os
 import json
 import time
-import logging
 from gridappsd import GridAPPSD
-from gridappsd.topics import service_input_topic, service_output_topic
-from cimgraph.databases import ConnectionParameters, BlazegraphConnection
+from cimgraph.databases.blazegraph import BlazegraphConnection
 
 from topology_processor.utils import DistributedTopologyMessage
 import cimgraph.data_profile.cimhub_2023 as cim
 from dotenv import load_dotenv
+
+# Uncomment and set following environment variables if testing outside GridAPPS-D container
+#os.environ["GRIDAPPSD_ADDRESS"] = ''
+#os.environ["GRIDAPPSD_PORT"] = ''
+#os.environ["GRIDAPPSD_USER"] = ''
+#os.environ["GRIDAPPSD_PASSWORD"] = ''
+#os.environ['CIMG_HOST'] = ''
+#os.environ['CIMG_PORT'] = ''
+#os.environ['CIMG_NAMESPACE'] = ''
+
+os.environ['CIMG_CIM_PROFILE'] = 'cimhub_2023'
+os.environ['CIMG_URL'] = 'http://blazegraph:8080/bigdata/namespace/kb/sparql'
+os.environ['CIMG_IEC61970_301'] = '8'
+
 
 class TopologyProcessor(GridAPPSD):
     
@@ -17,12 +29,9 @@ class TopologyProcessor(GridAPPSD):
         assert gapps.connected
         self.gapps = gapps
         self.log = self.gapps.get_logger()
-        db_url = f'http://{os.getenv("DB_ADDRESS","blazegraph")}:{os.getenv("DB_PORT","8080")}/bigdata/namespace/kb/sparql'
-        params = ConnectionParameters(url = db_url, cim_profile='cimhub_2023', iec61970_301=8)
-        self.blazegraph = BlazegraphConnection(params)
-
-        
+        self.blazegraph = BlazegraphConnection()
         self.log.info('Topology Background Service Started')
+        self.return_message = {}
 
     
     # GridAPPS-D service
@@ -33,24 +42,30 @@ class TopologyProcessor(GridAPPSD):
         
         if message['requestType'] == 'GET_DISTRIBUTED_AREAS':
             
-            self.log.info(f'Building Distributed Areas for {model_mrid}')
+            print(f'Building Distributed Areas for {model_mrid}')
+
+            if model_mrid in self.return_message and self.return_message[model_mrid] is not None:
+                print('Distributed Areas already built, sending cached message on '+reply_to)
+                self.gapps.send(reply_to, self.return_message[model_mrid])
             
-            topo_message = DistributedTopologyMessage()
-            container = self.blazegraph.get_object(mRID=model_mrid)
-
-            if isinstance(container, cim.Feeder):
-                topo_message.get_context_from_feeder(container, self.blazegraph)
-
-            elif isinstance(container, cim.FeederArea):
-                topo_message.get_context_from_feeder_area(container, self.blazegraph)
-                
-            elif isinstance(container, cim.DistributionArea):
-                topo_message.get_context_from_distribution_area(container, self.blazegraph)
-
-            return_message = json.dumps(topo_message.message, indent=4)
-            del topo_message
-            self.gapps.send(reply_to, return_message)
+            else:
             
+                topo_message = DistributedTopologyMessage()
+                container = self.blazegraph.get_object(mRID=model_mrid)
+
+                if isinstance(container, cim.Feeder):
+                    topo_message.get_context_from_feeder(container, self.blazegraph)
+
+                elif isinstance(container, cim.FeederArea):
+                    topo_message.get_context_from_feeder_area(container, self.blazegraph)
+                    
+                elif isinstance(container, cim.DistributionArea):
+                    topo_message.get_context_from_distribution_area(container, self.blazegraph)
+
+                self.return_message[model_mrid] = json.dumps(topo_message.message, indent=4)
+                del topo_message
+                self.gapps.send(reply_to, self.return_message[model_mrid])
+
         elif message['requestType'] == 'GET_BASE_TOPOLOGY':
             #Topology = self.get_base_topology(model_mrid)
             return_message = {  
